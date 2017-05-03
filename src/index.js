abe4ce06e47b08bae369cb9d3d9386349bc11146
newpouy <newpouy@gmail.com>
2017-05-03 22:10:52 +0900

    commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { App, Home, Login, Register, Wall } from 'containers';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+
+//const store = createStore(reducers, applyMiddleware(thunk));
+
+// const store = createStore(reducers, compose(
+//   applyMiddleware(thunk),
+//   window.devToolsExtension ? window.devToolsExtension() : f => f
+// ));
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+                <Route path="home" component={Home}/>
+                <Route path="login" component={Login}/>
+                <Route path="register" component={Register}/>
+                <Route path="wall/:username" component={Wall}/>
+            </Route>
+        </Router>
+    </Provider>, rootElement
+);
